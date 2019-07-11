510e05790306b34ab4d144f93441aec8d81ab672
Junhao Liao <junhao@junhao.ca>
2019-07-11 18:07:26 +0800

    Initial commit. Added tonalBuzzer.py

---
+++ tonalBuzzer.py
@@ -0,0 +1,33 @@
+from gpiozero import *
+from time import *
+
+tbz = TonalBuzzer(18) # tonal buzzer is attached to GPIO18
+bz = Buzzer(13)  # normal buzzer is attached to GPIO13
+
+twinkle_twinkle = [
+    "C4", "C4", "G4", "G4", "A4", "A4", "G4",
+    "F4", "F4", "E4", "E4", "D4", "D4", "C4",
+
+    "G4", "G4", "F4", "F4", "E4", "E4", "D4",
+    "G4", "G4", "F4", "F4", "E4", "E4", "D4",
+
+    "C4", "C4", "G4", "G4", "A4", "A4", "G4",
+    "F4", "F4", "E4", "E4", "D4", "D4", "C4",
+]
+
+def play_song(songName):
+    cnt = 0
+    for note in songName:
+        tbz.play(note)
+        sleep(0.15)
+        tbz.stop()
+        sleep(0.03)
+        cnt += 1
+        if (cnt == 7):
+            bz.on()
+            cnt = 0
+            sleep(0.15)
+            bz.off()
+
+
+play_song(twinkle_twinkle)
